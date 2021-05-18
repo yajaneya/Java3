1d5b1c3ab1430fffbde727c9292778ee48577c6a
yajaneya <janeya@yandex.ru>
2021-05-18 16:44:42 +0300

    Revert "Merge branch 'Dz3'"
    This reverts commit 597b978ac078ec6782c8180cb9e92dc387671062, reversing
    changes made to cec2c1305786385f2e927920e3c24276e4535496.

---
+++ scr/dz1/z3/Orange.java
@@ -0,0 +1,13 @@
+package dz1.z3;
+
+public class Orange extends Fruit {
+
+    public Orange() {
+        super(1.5f);
+    }
+
+    @Override
+    public String toString() {
+        return "Orange{}";
+    }
+}
